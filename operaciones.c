da712b31b7f687c9a9b313932c19cec28eafc30d
Antony Canseco <antonycanseco1232@gmail.com>
2025-09-01 16:18:29 -0600

    operaciones.c

--- operaciones.c
+++ operaciones.c
@@ -8,11 +8,7 @@ double division(double a, double b); // Daniel
 
 /* Implementaciones iniciales (a rellenar por cada integrante) */
 double suma(double a, double b) { return 0; }
-
-double resta(double a, double b) {
-    return a - b;
-}
-
+double resta(double a, double b) { return 0; }
 double multiplicacion(double a, double b) { return 0; }
 double division(double a, double b) { return 0; }
 
